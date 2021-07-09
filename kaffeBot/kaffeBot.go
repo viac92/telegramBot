38d9ff8fffaa403cb6dce09eef6be72ce4445049
viac92 <64146594+viac92@users.noreply.github.com>
2021-07-09 15:59:46 +0200

    miglioramenti ripetizioni

--- kaffeBot/kaffeBot.go
+++ kaffeBot/kaffeBot.go
@@ -7,20 +7,35 @@ import (
 	"time"
 	"unicode"
 	"strings"
+	"math/rand"
 
 	"github.com/yanzay/tbot/v2"
 )
 
+func randomNumber(n int) int {
+	rand.Seed(time.Now().UnixNano());
+	return rand.Intn(n)
+}
+
 //cammello data una strina la restituisce con lettere alternate minuscole maiuscole
 func cammello(s string) string {
 	var strOut string
-	for i,r := range s {
+	for _,r := range s {
 		if unicode.IsLetter(r) {
-			if i%2 == 0 {
-				strOut += string(unicode.ToUpper(r))
+			
+			if !strings.ContainsAny(string(r), "iI") {
+
+					random := randomNumber(2)
+					
+					if random == 0 {
+					strOut += string(unicode.ToUpper(r))
+					} else {
+					strOut += string(r)
+				}
 			} else {
 				strOut += string(r)
 			}
+
 		} else {
 			strOut += string(r)
 		}
@@ -48,38 +63,60 @@ func sostituisciCconK(s string) string {
 func moltiplica(s string) string {
 	var strOut string
 	rSli := []rune(s)
-
+	
 	for i := 0; i < len(rSli); i++ {
+		if strings.ContainsAny(string(rSli[i]), "aeoiuAEOIU") {
+			vocale := rSli[i]
+			rand := randomNumber(3)
+			for j := 0; j < rand; j++ {
+				strOut += string(vocale)
+			}
+		}
+
 		if rSli[i] == '!' {
-			for j := 0; j < 10; j++ {
-				if j%2 == 0 {
+			
+			rand := randomNumber(5)
+			for j := 0; j < rand + 5; j++ {
+
+				rand2 := randomNumber(5)
+				switch rand2 {
+				
+				case 0,1,2:
 					strOut += "!"
-				} else {
-					strOut += "?"
+				case 3:
+					strOut += "$"
+				case 4: 
+					strOut += "1"
 				}
 			}
-			continue
 		}
 
 		if rSli[i] == '?' {
-			for j := 0; j < 10; j++ {
-				if j%2 == 0 {
+			rand := randomNumber(5)
+			for j := 0; j < rand + 5; j++ {
+
+				rand2 := randomNumber(5)
+				switch rand2 {
+				
+				case 0,1,2:
 					strOut += "?"
-				} else {
-					strOut += "!"
+				case 3:
+					strOut += "/"
+				case 4: 
+					strOut += "1"
 				}
 			}
-			continue
 		}
 
 		if rSli[i] == '.' {
-			for j := 0; j < 3; j++ {
+			for j := 0; j < 10; j++ {
 				strOut += "."
 			}
 			continue
 		}
+
 		strOut += string(rSli[i])
-	}	
+	}
 	return strOut
 }
 
@@ -87,6 +124,7 @@ func moltiplica(s string) string {
 
 
 func main() {
+
 	fmt.Println("Sto trasmettendo...")
 	bot := tbot.New(os.Getenv("TELEGRAM_TOKEN2"))
 	c := bot.Client()
@@ -95,7 +133,7 @@ func main() {
 
 	bot.HandleMessage("kaffe .+", func(m *tbot.Message) {
 		text := strings.TrimPrefix(m.Text, "kaffe ")
-		nuovaFrase := moltiplica(sostituisciCconK(cammello(text)))
+		nuovaFrase := sostituisciCconK(cammello(moltiplica(text)))
 		c.SendChatAction(m.Chat.ID, tbot.ActionTyping)
 		time.Sleep(1 * time.Second)
 		
